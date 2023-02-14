afcfeefc0682bdec77383d399d7f59bfd2092470
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2023-02-14 17:38:17 -0400

    add exercise files

---
+++ agenda_insert.sql
@@ -0,0 +1,37 @@
+INSERT INTO agenda_usuario VALUES
+	(NULL, 'Gabriel Lima','lima.gabrieldsantos@gmail.com', 0, '12345678'),
+    (NULL, 'Guilherme Lima','gui@gmail.com', 0, '12345678'),
+    (NULL, 'Arthur Lima','arhur@gmail.com', 0, '12345678'),
+    (NULL, 'Julia Lima','ju@gmail.com', 0, '12345678'),
+    (NULL, 'Maria DB','maria@gmail.com', 0, '12345678');
+
+SELECT*FROM agenda_usuario;
+
+INSERT INTO agenda_eventos VALUES
+(NULL, 'Aula de RC1', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 1),
+(NULL, 'Aula de RC1', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 1),
+(NULL, 'Aula de MPS', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 1),
+(NULL, 'Aula de ED2', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 1),
+(NULL, 'Aula de PLP', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 1),
+(NULL, 'Aula de RC1', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 5),
+(NULL, 'Aula de ED2', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 5),
+(NULL, 'Aula de BD1', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 5),
+(NULL, 'Aula de ED2', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 5),
+(NULL, 'Aula de BD1', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 5),
+(NULL, 'Aula de PLP', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 4),
+(NULL, 'Aula de PLP', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 4),
+(NULL, 'Aula de RC1', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 4),
+(NULL, 'Aula de PLP', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 4),
+(NULL, 'Aula de BD1', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 4),
+(NULL, 'Aula de BD1', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 3),
+(NULL, 'Aula de ED2', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 3),
+(NULL, 'Aula de RC1', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 3),
+(NULL, 'Aula de PLP', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 3),
+(NULL, 'Aula de RC1', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 3),
+(NULL, 'Aula de MPS', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 2),
+(NULL, 'Aula de BD1', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 2),
+(NULL, 'Aula de RC1', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 2),
+(NULL, 'Aula de ED2', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 2),
+(NULL, 'Aula de MPS', '2023-01-23 10:00:56', 'Uma aula massa!', 0, 2);
+
+SELECT*FROM agenda_eventos;
